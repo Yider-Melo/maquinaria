816457af3916b761a7455b1d7abd3610a7c7d807
daniel <yidermelo123@gmail.com>
2026-07-11 01:30:01 -0500

    Fija iconos de Leaflet con URL absoluta para que el marcador se vea correctamente

--- frontend/src/app/machinery/map/map.ts
+++ frontend/src/app/machinery/map/map.ts
@@ -1,6 +1,14 @@
 import { Component, AfterViewInit, OnChanges, SimpleChanges, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 import * as L from 'leaflet';
 
+const iconDefault = L.icon({
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+  iconSize: [25, 41], iconAnchor: [12, 41], popupAnchor: [1, -34], shadowSize: [41, 41]
+});
+L.Marker.prototype.options.icon = iconDefault;
+
 @Component({
   selector: 'app-machinery-map',
   templateUrl: './map.html',
